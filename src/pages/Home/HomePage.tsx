bfe83388ec9dc85396836c327ef8e4fbb5b04490
yanaaarev <yannahrevellame@gmail.com>
2025-03-10 21:37:37 +0800

    rename images

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -2,8 +2,8 @@ import { useNavigate } from "react-router-dom";
 import N1 from "../../assets/senenartwork.png";
 import ARTEORK from "../../assets/ARTEORK.png";
 import CREATIFYElements111 from "../../assets/faqs.png";
-import WHATWEDO from "../../assets/CREATIFY-ELEMENTS-1-1.png";
-import CREATIFYElements131 from "../../assets/CREATIFY-ELEMENTS-1-3-1.png";
+import WHATWEDO from "../../assets/whatwedo.png";
+import CREATIFYElements131 from "../../assets/ayaa.png";
 
 export const HomePage = (): JSX.Element => {
   const navigate = useNavigate();
@@ -84,7 +84,7 @@ export const HomePage = (): JSX.Element => {
         </div>
 
         {/* Join Us Section */}
-        <div className="relative w-screen h-[450px] md:h-screen bg-[url('src/assets/ww-1.png')] bg-cover bg-center bg-no-repeat flex flex-col items-center justify-center pt-4 md:py-24 md:px-6">
+        <div className="relative w-screen h-[450px] md:h-screen bg-[url('../../assets/ww-1.png')] bg-cover bg-center bg-no-repeat flex flex-col items-center justify-center pt-4 md:py-24 md:px-6">
           <img className="w-40 md:w-[350px] mb-4 md:mb-6" src={CREATIFYElements131} alt="Creatify Elements" />
           <a
             href="https://docs.google.com/forms/d/e/1FAIpQLScbH3ZaaJ4Jg0BCt9_0Lb9x494b1BAgbvfwwmND2wCC-h0gYg/viewform?fbclid=IwY2xjawIsdtJleHRuA2FlbQIxMAABHedzb_U7flPO_QfStOIIDnKbpHExIzU2j95DnplGb2YCtZH0xde6B0-Pug_aem_O7DnW37x0Dp61xoYWsjBzA"
