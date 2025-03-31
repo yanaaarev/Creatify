b5c92c26d21c052173c294ca057e73bcdcdc09d3
yanaaarev <yannahrevellame@gmail.com>
2025-03-31 23:29:14 +0800

    implemented pic for saving

--- src/pages/Artist/ArtistDashboard.tsx
+++ src/pages/Artist/ArtistDashboard.tsx
@@ -205,6 +205,7 @@ const [currentMonth, setCurrentMonth] = useState<string>(new Date().toISOString(
 
   {/* ✅ Banner Holder (Fixed Height Issue) */}
   <div className="w-full mt-[93px] h-[200px] md:w-[1250px] md:h-[290px] relative">
+  <div className="absolute inset-0 bg-black bg-opacity-40 img-overlay"></div>
     <img
       className="w-full h-full object-cover"
       src={artist?.bannerImage || DEFAULT_BANNER_URL} // Temporary Default Banner
@@ -216,7 +217,8 @@ const [currentMonth, setCurrentMonth] = useState<string>(new Date().toISOString(
 <div className="relative flex flex-col items-center w-full max-w-[800px] p-4">
   {/* Profile Picture Holder */}
   <div className="w-[160px] h-[160px] md:w-[240px] md:h-[240px] lg:w-[260px] lg:h-[260px] rounded-full overflow-hidden absolute left-1/2 transform -translate-x-1/2 -top-20 md:-top-28 lg:-top-32">
-  <div className="img-overlay"></div>
+  {/* Overlay */}
+  <div className="absolute inset-0 bg-black bg-opacity-40 img-overlay rounded-full"></div>
     <img
       className="w-full h-full object-cover"
       src={artist?.profilePicture || DEFAULT_AVATAR_URL} // Temporary Default Avatar
